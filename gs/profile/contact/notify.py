7949aaa960289bf93ae017e6e6f16c04c8a54855
Michael JasonSmith <mpj17@onlinegroups.net>
2016-06-20 14:48:24 -0700

    Making the start on the notifier

---
+++ gs/profile/contact/notify.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2016 Michael JasonSmith and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals, print_function
+from gs.content.email.base import NotifierABC
+
+
+class RequestNotifier(NotifierABC):
+    htmlTemplateName = 'gs-profile-contact.html'
+    textTemplateName = 'gs-profile-contact.txt'
+
+    def notify(self, userInfo):
+        subject = 'Contact requested'
+        text = self.textTemplate()
+        html = self.htmlTemplate()
+
+        sender = MessageSender(self.context, userInfo)
+        sender.send_message(subject, text, html)
+        self.reset_content_type()
+
